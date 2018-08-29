9e0c1686a58420a48f24e04c33cf1a6fdbaf5f26
Eugene St <Eugene.stepchenko@gmail.com>
2018-08-29 19:08:30 +0300

    Not final version

---
+++ AssignTitlesTest/Game.swift
@@ -0,0 +1,83 @@
+import Foundation
+import GameKit
+
+struct Game
+{
+    var questions = [Question]() // To collect all questions in one array
+    
+    var collectedIndexes = [Int]() // Will be used to randomize Answers
+    
+    var questionToDisplay: Question // To display current question
+    
+    var correctAnswers = 0 // Will catch all correct answers
+    
+    var questionsAsked = 0 // Will catch ALL answers
+    
+    var selectedIndex = 0
+    
+    var correctAnswer = ""
+    
+    let questionsPerRound = 5
+    
+    let question1 = Question(question: "Four stories about a honey-loving bear and his friends", answerChoices: ["Dumbo", "Bolt", "Oliver and Company", "The many adventures of Winnie the Pooh"], correctAnswer: "The many adventures of Winnie the Pooh")
+    
+    let question2 = Question(question: "A wooden puppet needs to be brave, truthful and unselfish to become human", answerChoices: ["Brave", "Peter Pan", "Saludos Amigos", "Pinocchio"], correctAnswer: "Pinocchio")
+    
+    let question3 = Question(question: "Two creatures try to return a young girl back to her parellel world", answerChoices: ["The Rescuers Down Under", "Monsters, Inc.", "A Bug's Life", "The Incredibles"], correctAnswer: "Monsters, Inc.")
+    
+    let question4 = Question(question: "A girl has several adventures after following a rabbit into a rabbit hole", answerChoices: ["Pocahontas", "The Little Marmaid", "Alice in Worderland", "Cinderella"], correctAnswer: "Alice in Worderland")
+    
+    let question5 = Question(question: "A mean and wealthy woman goes a long way to get a fur coat", answerChoices: ["The Emperor's New Groove", "Show WHite and the Sever Dwarfs", "One Hundred and One Dalmatians", "Beauty and the Beast"], correctAnswer: "One Hundred and One Dalmatians")
+    
+    let question6 = Question(question: "A man raised by gorillas falls in love with the daughter of an English explorer", answerChoices: ["Beauty and the Beast", "Tarzan", "The Jungle Book", "Peter Pan"], correctAnswer: "Tarzan")
+    
+    let question7 = Question(question: "The arrival of a new birthday present leads to jealousy and adventures while moving to a new house", answerChoices: ["Toy Story", "Finding Nemo", "The Aristocats", "The Many Adventures of Winnie the Pooh"], correctAnswer: "Toy Story")
+    
+    let question8 = Question(question: "A leader is killed by his brother and is later challenged by the leader's son", answerChoices: ["The Sword in the Stone", "The Lion King", "The Jungle Book", "Frozen"], correctAnswer: "The Lion King")
+    
+    let question9 = Question(question: "A TV show dog thinks he has super powers and tries to find the girl that plays his owner", answerChoices: ["Wreck-it Ralph", "Finding Nemo", "Up", "Bolt"], correctAnswer: "Bolt")
+    
+    let question10 = Question(question: "A teenage princess is tricked to touch an enchanted spinning wheel", answerChoices: ["Sleeping Beauty", "Cinderella", "Snow White and the Seven Dwarfs", "Tangled"], correctAnswer: "Sleeping Beauty")
+    
+    init()
+    {
+        questions = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10]
+        questionToDisplay = question1
+    }
+    
+    mutating func generateQuestion()
+    {
+        /*
+ selectedIndex = GKRandomSource.sharedRandom().nextInt(upperBound: questions.count)
+        
+        while collectedIndexes.contains(selectedIndex)
+        {
+            selectedIndex = GKRandomSource.sharedRandom().nextInt(upperBound: questions.count)
+        }
+        
+        collectedIndexes.append(selectedIndex)
+        questionToDisplay = questions[selectedIndex]
+        correctAnswer = questions[selectedIndex].correctAnswer
+        */
+        
+        repeat {
+            selectedIndex = GKRandomSource.sharedRandom().nextInt(upperBound: questions.count)
+        } while questions.count == selectedIndex
+        
+        questionToDisplay = questions[selectedIndex]
+        correctAnswer = questions[selectedIndex].correctAnswer
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- AssignTitlesTest/ViewController.swift
+++ AssignTitlesTest/ViewController.swift
@@ -7,19 +7,165 @@
 //
 
 import UIKit
+import GameKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textPosition: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    
+    @IBOutlet weak var correctIncorrect: UILabel!
+    
+    var buttons = [UIButton]()
+    var game = Game()
+    var timer = Timer()
+    let intervalInSeconds = 15
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        buttons = [button1, button2, button3, button4]
+        defaultUiView()
+        displayQuestion()
+        game.collectedIndexes = []
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func displayQuestion() // DOES NOT WORK !!!
+    {
+        game.generateQuestion()
+        textPosition.text = game.questionToDisplay.question
+        //displayButtonTitles()
+        correctIncorrect.isHidden = true
     }
+    
+    func displayButtonTitles()
+    {
+        
+        for button in buttons
+        {
+            var questionToDisplay = game.questionToDisplay
+            var index = GKRandomSource.sharedRandom().nextInt(upperBound: questionToDisplay.answerChoices.count)
+            
+            while game.collectedIndexes.contains(index)
+            {
+                index = GKRandomSource.sharedRandom().nextInt(upperBound: questionToDisplay.answerChoices.count)
+            }
+            
+            game.collectedIndexes.append(index)
+            
+            button.setTitle(questionToDisplay.answerChoices[index], for: .normal)
+        }
+    }
+    
+    @IBAction func checkAnswers(_ sender: UIButton) {
+        correctIncorrect.isHidden = false
+        game.questionsAsked += 1
+        print("Questions Asked \(game.questionsAsked)") // PRINT
+        if sender.currentTitle == game.questionToDisplay.correctAnswer
+        {
+            sender.flash()
+            correctIncorrect.text = "Correct"
+            correctIncorrect.textColor = UIColor.green
+            game.correctAnswers += 1
+            print("Correct Answers \(game.correctAnswers)") // PRINT
+            sender.backgroundColor = UIColor.green
+        }
+            else
+        {
+            sender.shake()
+            sender.backgroundColor = UIColor.red
+           correctIncorrect.text = "Incorrect"
+            correctIncorrect.textColor = UIColor.red
+            print("Incorrect Answer") // PRINT
+            
+        }
+        
+        loadNextRoundWithDelay(seconds: 2)
+        
+    }
+    
+    @IBAction func nextQuestionCheck() {
+        displayQuestion()
+        print("Previous index is - \(game.collectedIndexes)")
+        print("Selected index is - \(game.selectedIndex)")
 
-
+    }
+    
+    
+    func defaultUiView()
+    {
+        textPosition.textColor = UIColor.white
+        correctIncorrect.textColor = UIColor.white
+        for button in buttons
+        {
+            button.layer.cornerRadius = 15
+            button.setTitleColor(UIColor.white, for: .normal)
+        }
+    }
+    
+    func displayScore()
+    {
+        for button in buttons
+        {
+            button.isHidden = true
+        }
+        
+        textPosition.text = "Great, you have finished. Your score is \(game.correctAnswers) of \(game.questionsPerRound)"
+        correctIncorrect.isHidden = true
+    }
+    
+    func nextRound()
+    {
+        print("Timer works")
+        /*
+         if game.questionsAsked == game.questionsPerRound
+        {
+            displayScore()
+        } else {
+         */
+            displayQuestion()
+        //}
+    
+    }
+    
+    func loadNextRoundWithDelay(seconds: Int) {
+        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
+        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
+        // Calculates a time value to execute the method given current time and delay
+        let dispatchTime = DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)
+        
+        // Executes the nextRound method at the dispatch time on the main queue
+        DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
+            self.nextRound()
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
